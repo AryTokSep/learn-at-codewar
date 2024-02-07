6ec26a40ec42c6a17c3dc85a6861be1a0f126b18
AryTokSep <arytokseptiawan@gmail.com>
2024-02-07 22:38:20 +0700

    reversed sequence

---
+++ 2-reverseSeq.js
@@ -0,0 +1,23 @@
+// reverse sequence
+
+const reverseSeq = (n) => {
+    const result = [];
+
+    for (let i = n; i >= 1; i--) {
+        result.push(i);
+    }
+    return result;
+  };
+
+ 
+
+
+// const reverseSeq = (n) => {
+//     return Array(n)
+//     .fill()
+//     .map((el, i)=> i + 1)
+//     .reverse();
+//   };
+
+
+  console.log(reverseSeq(5));
