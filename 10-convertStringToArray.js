c77a9cceaea3c1a958916b81074de5f2ce58543f
AryTokSep <arytokseptiawan@gmail.com>
2024-03-18 22:06:15 +0700

    Convert a string to an array

---
+++ 10-convertStringToArray.js
@@ -0,0 +1,14 @@
+// buatlah sebuah fungsi untuk memecah(split) 
+// sebuah string atau kalimat dan convert menjadi array of words
+// atau menjadi kata-kata di dalam kalimat yang panjang tersebut per kata.
+
+
+function stringToArray(string){  
+    return string.split(' ')
+}
+
+
+
+// cara kedua
+
+// const stringToArray = (string) => string.split(' ');
